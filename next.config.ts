5d1e080aa008832d2ebdf97c9d47e14e93da06d9
avias8 <HarshvardhanV98@gmail.com>
2025-07-07 20:39:14 -0600

    fix(next.config): add missing pathname for remote pattern in image configuration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
+        pathname: "/weddingguestphotos/**",
       },
       {
         protocol: "https",
